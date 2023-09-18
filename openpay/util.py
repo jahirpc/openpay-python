c24cf56784f2478a0739b9195033371e3621f1f8
JahirPC <jahirpc@gmail.com>
2023-09-17 21:39:23 -0500

    inicial8

--- openpay/util.py
+++ openpay/util.py
@@ -8,6 +8,8 @@ __all__ = ['utf8']
 
 
 def utf8(value):
+    if sys.version_info[0] >= 3:
+        unicode = str
     if isinstance(value, unicode) and sys.version_info < (3, 0):
         return value.encode('utf-8')
     else:
